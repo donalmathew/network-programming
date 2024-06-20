e342d4bc209340ee7a16d1a7ffc4bb044c4bb86f
Donal Mathew <donalmathewpt@gmail.com>
2024-06-20 14:08:26 +0530

    temp updated

--- sct/client.c
+++ sct/client.c
@@ -5,19 +5,35 @@
 #include<sys/types.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+#include <unistd.h>
+
 void main()
 {
-struct sockaddr_in serveraddr;
-char buffer[1024];
-int clientsocket = socket(AF_INET,SOCK_STREAM,0);
-printf("client socket created successfully \n");
-serveraddr.sin_family = AF_INET;
-serveraddr.sin_port = htons(4444);
-serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-connect(clientsocket,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
-printf("connected to server \n");
-strcpy(buffer,"hello from client");
-send(clientsocket,buffer,strlen(buffer),0);
-recv(clientsocket,buffer,1024,0);
-printf("data received: %s \n",buffer);
+    // Define the server address structure and buffer for communication
+    struct sockaddr_in serveraddr;
+    char buffer[1024];
+
+    // Create a socket for the client
+    int clientsocket = socket(AF_INET, SOCK_STREAM, 0);
+    printf("client socket created successfully \n");
+
+    // Specify server address and port
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(4444);
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Connect to the server
+    connect(clientsocket, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    printf("connected to server \n");
+
+    // Prepare and send data to the server
+    strcpy(buffer, "hello from client");
+    send(clientsocket, buffer, strlen(buffer), 0);
+
+    // Receive data from the server
+    recv(clientsocket, buffer, 1024, 0);
+    printf("data received: %s \n", buffer);
+
+    // Close the client socket
+    close(clientsocket);
 }
--- sct/server.c
+++ sct/server.c
@@ -5,23 +5,43 @@
 #include<sys/types.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+#include <unistd.h>
+
 void main()
 {
-struct sockaddr_in serveraddr,clientaddr;
-char buffer[1024];
-int serversocket = socket(AF_INET,SOCK_STREAM,0);
-printf("server socket created successfully \n");
-serveraddr.sin_family = AF_INET;
-serveraddr.sin_port = htons(4444);
-serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-bind(serversocket,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
-printf("bind to port number \n");
-listen(serversocket,6);
-printf("listening------------\n");
-int addrsize = sizeof(clientaddr);
-int newsocket=accept(serversocket,(struct sockaddr *)&clientaddr,&addrsize);
-recv(newsocket,buffer,1024,0);
-printf("data received: %s \n",buffer);
-strcpy(buffer,"hello from server");
-send(newsocket,buffer,strlen(buffer),0);
+    // Define the server and client address structures and buffer for communication
+    struct sockaddr_in serveraddr, clientaddr;
+    char buffer[1024];
+
+    // Create a socket for the server
+    int serversocket = socket(AF_INET, SOCK_STREAM, 0);
+    printf("server socket created successfully \n");
+
+    // Specify server address and port
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(4444);
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Bind the socket to the specified address and port
+    bind(serversocket, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    printf("bind to port number \n");
+
+    // Listen for incoming connections
+    listen(serversocket, 6);
+    printf("listening------------\n");
+
+    // Accept an incoming connection
+    int addrsize = sizeof(clientaddr);
+    int newsocket = accept(serversocket, (struct sockaddr*)&clientaddr, &addrsize);
+
+    // Receive data from the client
+    recv(newsocket, buffer, 1024, 0);
+    printf("data received: %s \n", buffer);
+
+    // Prepare and send data to the client
+    strcpy(buffer, "hello from server");
+    send(newsocket, buffer, strlen(buffer), 0);
+
+    // Close the new socket and the server socket
+    close(newsocket);
 }
