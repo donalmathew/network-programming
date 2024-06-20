86438451d09e6d39c407dc3f33741e20bf088925
Donal Mathew <donalmathewpt@gmail.com>
2024-06-20 14:31:01 +0530

    udp completed

--- udp/client.c
+++ udp/client.c
@@ -6,26 +6,37 @@
 # include<sys/socket.h>
 # include<netinet/in.h>
 # include<arpa/inet.h>
+
 int main()
 {
-char * hello = "hello from client";
-struct sockaddr_in serveraddr;
-int clientsocket = socket(AF_INET,SOCK_DGRAM,0);
-if(clientsocket == -1)
-{
-perror("socket error \n");
-exit(1);
-}
-printf("server socket created \n");
-serveraddr.sin_family = AF_INET;
-serveraddr.sin_port = htons(5555);
-serveraddr.sin_addr.s_addr = INADDR_ANY;
-int len = sendto(clientsocket, (char*)hello,strlen(hello),0,(struct
-sockaddr*)&serveraddr,sizeof(serveraddr));
-if(len == -1)
-{
-perror("failed to send \n");
-}
-close(clientsocket);
-return 0;
+    // Message to be sent to the server
+    char * hello = "hello from client";
+
+    // Define server address structure
+    struct sockaddr_in serveraddr;
+
+    // Create a UDP socket
+    int clientsocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if(clientsocket == -1)
+    {
+        perror("socket error \n");
+        exit(1);
+    }
+    printf("client socket created \n");
+
+    // Specify server address and port
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(5555);
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Send data to the server
+    int len = sendto(clientsocket, (char*)hello, strlen(hello), 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    if(len == -1)
+    {
+        perror("failed to send \n");
+    }
+
+    // Close the client socket
+    close(clientsocket);
+    return 0;
 }
--- udp/server.c
+++ udp/server.c
@@ -6,30 +6,44 @@
 # include<sys/socket.h>
 # include<netinet/in.h>
 # include<arpa/inet.h>
+
 int main()
 {
-char buffer[50];
-struct sockaddr_in servaddr;
-int serversocket = socket(AF_INET,SOCK_DGRAM,0);
-if(serversocket == -1)
-{
-perror("socket error \n");
-exit(1);
-}
-printf("server socket created \n");
-servaddr.sin_family = AF_INET;
-servaddr.sin_port = htons(5555);
-servaddr.sin_addr.s_addr = INADDR_ANY;
-int rc = bind(serversocket,(struct sockaddr*)&servaddr,sizeof(servaddr));
-if(rc == -1)
-{
-perror("bind error \n");
-exit(1);
-}
-printf("bind to port number \n");
-socklen_t len = 0;
-int n = recvfrom(serversocket, (char *)buffer,50,MSG_WAITALL,0,&len);
-printf("%s",buffer);
-close(serversocket);
-return 0;
+    // Buffer to store received data
+    char buffer[50];
+
+    // Define server address structure
+    struct sockaddr_in servaddr;
+
+    // Create a UDP socket
+    int serversocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if(serversocket == -1)
+    {
+        perror("socket error \n");
+        exit(1);
+    }
+    printf("server socket created \n");
+
+    // Specify server address and port
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(5555);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind the socket to the specified address and port
+    int rc = bind(serversocket, (struct sockaddr*)&servaddr, sizeof(servaddr));
+    if(rc == -1)
+    {
+        perror("bind error \n");
+        exit(1);
+    }
+    printf("bind to port number \n");
+
+    // Receive data from the client
+    socklen_t len = 0;
+    int n = recvfrom(serversocket, (char *)buffer, 50, MSG_WAITALL, 0, &len);
+    printf("%s", buffer);
+
+    // Close the server socket
+    close(serversocket);
+    return 0;
 }
