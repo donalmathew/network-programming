0feaeff00a53409107e8271f88296e269421d283
Donal Mathew <donalmathewpt@gmail.com>
2024-06-21 11:23:01 +0530

    3 chat completed

--- 3.chat/client.c
+++ 3.chat/client.c
@@ -6,51 +6,55 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
 #define PORT 6666
+
 int main()
 {
-int clientsocket, ret;
-struct sockaddr_in serveraddr;
-char buffer[1024];
-clientsocket = socket(AF_INET, SOCK_STREAM, 0);
-if(clientsocket<0)
-{
-perror("socket failed \n");
-exit(EXIT_FAILURE);
-}
-printf("socket created successfully \n");
-memset(&serveraddr,'\0',sizeof(serveraddr));
-serveraddr.sin_family = AF_INET;
-serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-serveraddr.sin_port = htons(PORT);
-ret=connect(clientsocket, (struct sockaddr*)&serveraddr,
-sizeof(serveraddr));
-if(ret<0)
-{
-printf("error in connection \n");
-exit(EXIT_FAILURE);
-}
-printf("connected to server \n");
-while(1)
-{
-printf("client \t");
-scanf("%s",buffer);
-send(clientsocket,buffer,strlen(buffer),0);
-if(strcmp(buffer,":exit")==0)
-{
-close(clientsocket);
-printf("disconnected from server \n");
-exit(1);
-}
-if(recv(clientsocket,buffer,1024,0)<0)
-{
-printf("error in receving data \n");
-}
-else
-{
-printf("server: \t%s\n",buffer);
-}
+    int clientsocket, ret;
+    struct sockaddr_in serveraddr;
+    char buffer[1024];
 
-}
-return 0;
+    // Create a TCP socket
+    clientsocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientsocket < 0) {
+        perror("socket failed \n");
+        exit(EXIT_FAILURE);
+    }
+    printf("socket created successfully \n");
+
+    // Initialize server address structure
+    memset(&serveraddr, '\0', sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serveraddr.sin_port = htons(PORT);
+
+    // Connect to the server
+    ret = connect(clientsocket, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    if (ret < 0) {
+        printf("error in connection \n");
+        exit(EXIT_FAILURE);
+    }
+    printf("connected to server \n");
+
+    while (1) {
+        // Read message from user input
+        printf("client \t");
+        scanf("%s", buffer);
+        // Send message to the server
+        send(clientsocket, buffer, strlen(buffer), 0);
+        if (strcmp(buffer, ":exit") == 0) {
+            // Close the socket and exit if the exit command is received
+            close(clientsocket);
+            printf("disconnected from server \n");
+            exit(1);
+        }
+        // Receive message from the server
+        if (recv(clientsocket, buffer, 1024, 0) < 0) {
+            printf("error in receiving data \n");
+        } else {
+            printf("server: \t%s\n", buffer);
+        }
+    }
+    return 0;
 }
--- 3.chat/server.c
+++ 3.chat/server.c
@@ -6,68 +6,73 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
 #define PORT 6666
+
 int main()
 {
-int sockfd,ret;
-struct sockaddr_in serveraddr;
-int newsocket;
-struct sockaddr_in newaddr;
-socklen_t addr_size;
-char buffer[1024];
-pid_t childpid;
-sockfd = socket(AF_INET, SOCK_STREAM, 0);
-if(sockfd<0)
-{
-printf("\n Socket creation error \n");
-exit(1);
-}
-printf("socket is created \n");
-memset(&serveraddr,'\0',sizeof(serveraddr));
-serveraddr.sin_family = AF_INET;
-serveraddr.sin_port = htons(PORT);
-serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-ret = bind(sockfd, (struct sockaddr*)&serveraddr,
-sizeof(serveraddr));
-if(ret<0)
-{
-printf("error in binding \n");
-exit(1);
-}
-printf("binding successful \n");
-listen(sockfd,6);
-printf("listening------------");
-while(1)
-{
-newsocket=accept(sockfd,(struct sockaddr*)&newaddr,&addr_size);
-if(newsocket<0)
-{
-exit(1);
-}
-printf("connection accepted from %s:%d\
-n",inet_ntoa(newaddr.sin_addr),ntohs(newaddr.sin_port));
-if((childpid=fork())==0)
-{
+    int sockfd, ret;
+    struct sockaddr_in serveraddr;
+    int newsocket;
+    struct sockaddr_in newaddr;
+    socklen_t addr_size;
+    char buffer[1024];
+    pid_t childpid;
 
-close(sockfd);
-while(1)
-{
-recv(newsocket,buffer,1024,0);
-if(strcmp(buffer,":exit")==0)
-{
-printf("disconnectd from %s:%d\
-n",inet_ntoa(newaddr.sin_addr),ntohs(newaddr.sin_port));
-break;
-}
-else
-{
-printf("client: %s\n",buffer);
-send(newsocket,buffer,strlen(buffer),0);
-bzero(buffer,sizeof(buffer));
-}
-}
-}
-}
-close(newsocket);
-return 0;
+    // Create a TCP socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        printf("\n Socket creation error \n");
+        exit(1);
+    }
+    printf("socket is created \n");
+
+    // Initialize server address structure
+    memset(&serveraddr, '\0', sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PORT);
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Bind the socket to the specified IP and port
+    ret = bind(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    if (ret < 0) {
+        printf("error in binding \n");
+        exit(1);
+    }
+    printf("binding successful \n");
+
+    // Listen for incoming connections
+    listen(sockfd, 6);
+    printf("listening------------");
+
+    while (1) {
+        // Accept an incoming connection
+        newsocket = accept(sockfd, (struct sockaddr*)&newaddr, &addr_size);
+        if (newsocket < 0) {
+            exit(1);
+        }
+        printf("connection accepted from %s:%d\n", inet_ntoa(newaddr.sin_addr), ntohs(newaddr.sin_port));
+
+        // Create a child process to handle the new connection
+        if ((childpid = fork()) == 0) {
+            // In the child process, close the listening socket
+            close(sockfd);
+            while (1) {
+                // Receive messages from the client
+                recv(newsocket, buffer, 1024, 0);
+                if (strcmp(buffer, ":exit") == 0) {
+                    printf("disconnected from %s:%d\n", inet_ntoa(newaddr.sin_addr), ntohs(newaddr.sin_port));
+                    break;
+                } else {
+                    printf("client: %s\n", buffer);
+                    // Echo the message back to the client
+                    send(newsocket, buffer, strlen(buffer), 0);
+                    bzero(buffer, sizeof(buffer));
+                }
+            }
+        }
+    }
+    // Close the new socket (this is only reached in the parent process)
+    close(newsocket);
+    return 0;
 }
